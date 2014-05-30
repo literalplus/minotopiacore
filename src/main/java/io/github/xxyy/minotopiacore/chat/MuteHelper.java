6b63689bb12d3c2ddb7c662004750c157b8e0129
xxyy <devnull@nowak-at.net>
2014-05-30 17:12:55 +0200

    Change stuff up to allow for badly-written commit messages. Last time, I promise.

--- src/main/java/io/github/xxyy/minotopiacore/chat/MuteHelper.java
+++ src/main/java/io/github/xxyy/minotopiacore/chat/MuteHelper.java
@@ -1,57 +1,61 @@
 package io.github.xxyy.minotopiacore.chat;
 
+import org.bukkit.configuration.file.YamlConfiguration;
+
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Set;
 
-import org.bukkit.configuration.file.YamlConfiguration;
-
 public class MuteHelper {
-	public static final File muteCfgFile = new File("plugins/MinoTopiaCore","mute.lst.yml");
-	public static final YamlConfiguration muteCfg = YamlConfiguration.loadConfiguration(muteCfgFile);
-	public static boolean isPlayerMuted(String plrName){
-		return muteCfg.contains("muted."+plrName);
-	}
-	public static void mutePlayer(String plrName,String reason,String senderName){
-		if(!isPlayerMuted(plrName)){
-			//muteCfg.set("muted."+plrName, true);
-			muteCfg.set("muted."+plrName+".reason", reason);
-			muteCfg.set("muted."+plrName+".mutedby", senderName);
-			muteCfg.set("muted."+plrName+".mutetime", (new SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().getTime())));
-			try {
-				muteCfg.save(muteCfgFile);
-			} catch (IOException e) {
-				System.out.println("§cIOException when trying to save mute file :(");
-				e.printStackTrace();
-			}
-		}
-	}
-	public static void unmutePlayer(String plrName){
-		if(isPlayerMuted(plrName)){
-			muteCfg.set("muted."+plrName, null);
-			try {
-				muteCfg.save(muteCfgFile);
-			} catch (IOException e) {
-				System.out.println("§cIOException when trying to save mute file :(");
-				e.printStackTrace();
-			}
-		}
-	}
-	public static Set<String> getMutedPlayerPaths(){
-		return muteCfg.getConfigurationSection("muted").getKeys(false);
-	}
-//	public static String getPlayerNameByPath(String path){
-//		//return path.replace("muted.", "");
-//	}
-	public static String getReasonByPath(String path){
-		return muteCfg.getString("muted."+path+".reason");
-	}
-	public static String getMuterByPath(String path){
-		return muteCfg.getString("muted."+path+".mutedby");
-	}
-	public static String getMuteTimeByPath(String path){
-		return muteCfg.getString("muted."+path+".mutetime");
-	}
+    public static final File muteCfgFile = new File("plugins/MinoTopiaCore", "mute.lst.yml");
+    public static final YamlConfiguration muteCfg = YamlConfiguration.loadConfiguration(muteCfgFile);
+
+    public static boolean isPlayerMuted(String plrName) {
+        return muteCfg.contains("muted." + plrName);
+    }
+
+    public static void mutePlayer(String plrName, String reason, String senderName) {
+        if (!isPlayerMuted(plrName)) {
+            //muteCfg.set("muted."+plrName, true);
+            muteCfg.set("muted." + plrName + ".reason", reason);
+            muteCfg.set("muted." + plrName + ".mutedby", senderName);
+            muteCfg.set("muted." + plrName + ".mutetime", (new SimpleDateFormat("yyyy-MM-dd HH:mm").format(Calendar.getInstance().getTime())));
+            try {
+                muteCfg.save(muteCfgFile);
+            } catch (IOException e) {
+                System.out.println("§cIOException when trying to save mute file :(");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void unmutePlayer(String plrName) {
+        if (isPlayerMuted(plrName)) {
+            muteCfg.set("muted." + plrName, null);
+            try {
+                muteCfg.save(muteCfgFile);
+            } catch (IOException e) {
+                System.out.println("§cIOException when trying to save mute file :(");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static Set<String> getMutedPlayerPaths() {
+        return muteCfg.getConfigurationSection("muted").getKeys(false);
+    }
+
+    public static String getReasonByPath(String path) {
+        return muteCfg.getString("muted." + path + ".reason");
+    }
+
+    public static String getMuterByPath(String path) {
+        return muteCfg.getString("muted." + path + ".mutedby");
+    }
+
+    public static String getMuteTimeByPath(String path) {
+        return muteCfg.getString("muted." + path + ".mutetime");
+    }
 }
