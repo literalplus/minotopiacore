496c8f2fab11aebaf2a4d0e73d2a8a3ec8227d56
xxyy <devnull@nowak-at.net>
2014-06-11 00:09:36 +0200

    Refactor CommandTeam to support the newest version of PEx and object-orientation principles with #LAMBAPOWER

--- src/main/java/io/github/xxyy/minotopiacore/misc/cmd/CommandTeam.java
+++ src/main/java/io/github/xxyy/minotopiacore/misc/cmd/CommandTeam.java
@@ -1,14 +1,14 @@
 package io.github.xxyy.minotopiacore.misc.cmd;
 
-import io.github.xxyy.common.util.PlayerHelper;
-import io.github.xxyy.minotopiacore.ConfigHelper;
 import io.github.xxyy.minotopiacore.MTC;
 import io.github.xxyy.minotopiacore.MTCCommandExecutor;
 import io.github.xxyy.minotopiacore.helper.MTCHelper;
-import org.bukkit.ChatColor;
+import org.apache.commons.lang.Validate;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import ru.tehkode.permissions.PermissionManager;
+import org.bukkit.entity.Player;
+import ru.tehkode.permissions.PermissionGroup;
 import ru.tehkode.permissions.PermissionUser;
 import ru.tehkode.permissions.bukkit.PermissionsEx;
 
@@ -16,62 +16,182 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class CommandTeam extends MTCCommandExecutor {
-    private static final HashMap<String, ArrayList<String>> groups = new HashMap<>();
-    private static final HashMap<String, String> groupPrefixes = new HashMap<>();
+    private final List<TeamGroup> groups = new ArrayList<>();
+    private final MTC plugin;
+
+    public CommandTeam(MTC plugin) {
+        this.plugin = plugin;
+    }
 
     @Override
     public boolean catchCommand(CommandSender sender, String senderName, Command cmd, String label, String[] args) {
         MTCHelper.sendLoc("XU-teamheader", sender, false);
-        if (CommandTeam.groups.isEmpty()) {
+        if (this.groups.isEmpty()) {
             boolean overridePrefixes = MTC.instance().getConfig().getBoolean("team.overridePrefixes");
             try {
                 if (!PermissionsEx.isAvailable()) {
                     sender.sendMessage("§cPermissionsEx ist nicht verfügbar.");
                     return true;
                 }
-                PermissionManager pMan = PermissionsEx.getPermissionManager();
 
-                for (String groupName : ConfigHelper.getTeamGroupsInOrder()) {
-                    ArrayList<String> userNames = new ArrayList<>();
-                    Set<PermissionUser> users = pMan.getUsers(groupName, false);
-                    userNames.addAll(users.stream().map(PermissionUser::getName).collect(Collectors.toList()));
-                    CommandTeam.groups.put(groupName, userNames);
-                    if (overridePrefixes) {
-                        CommandTeam.groupPrefixes.put(groupName, ChatColor.translateAlternateColorCodes('&', ConfigHelper.getTeamMap().get(groupName)));
-                    } else {
-                        CommandTeam.groupPrefixes.put(groupName, ChatColor.translateAlternateColorCodes('&', pMan.getGroup(groupName).getPrefix()));
-                    }
-                }
-            } catch (Exception e) {
+                PermissionsEx.getPermissionManager().getGroupList().stream()
+                        .filter(group -> group.getOptionBoolean("team", null, false))
+                        .forEach(group -> this.groups.add(new TeamGroup(group)));
+            } catch (Throwable e) {
                 sender.sendMessage("§cFehler beim Holen der Teammitglieder.");
                 e.printStackTrace();
                 System.out.println("PermissionsEx Hook failed. (CommandTeam)");
                 return true;
             }
         }
-        Set<String> groupKeys = CommandTeam.groups.keySet();
-        for (int i = 0; i < groupKeys.size(); i++) {
-            String groupName = ConfigHelper.getTeamGroupsInOrder().get(i);
-            List<String> members = CommandTeam.groups.get(groupName);
-            String groupPrefix = CommandTeam.groupPrefixes.get(groupName);
-            String seperator = MTCHelper.loc("XU-teamseperator", senderName, false);
-            Iterator<String> it = members.iterator();
-            if (!it.hasNext()) {
-                continue;
-            }
-            String line = groupPrefix + " " + CommandTeam.getPlayerString(it.next(), senderName);
-            while (it.hasNext()) {
-                line += seperator + CommandTeam.getPlayerString(it.next(), senderName);
-            }
-            sender.sendMessage(line);
-        }
+
+        List<TeamMember> allMembers = new LinkedList<>();
+        this.groups.stream().forEach((grp) -> allMembers.addAll(grp.getMembers()));
+
+        Arrays.asList(Bukkit.getOnlinePlayers()).stream()
+                .forEach((plr) -> allMembers.parallelStream()
+                        .forEach((member) -> member.checkMatch(plr)
+                        )); //Only loop through online players once
+
+        this.groups.stream().forEach(group -> sender.sendMessage(group.niceRepresentation()));
+
         return true;
     }
 
-    private static String getPlayerString(String plrName, String senderName) {
-        return MTCHelper.locArgs("XU-team" +
-                (PlayerHelper.isOnlineIgnoreCase(plrName) ? "on" : "off") +
-                "line", senderName, false, plrName);
+    private static class TeamMember {
+        private final UUID uuid;
+        private boolean lastOnline = false;
+        private String lastName;
+
+        public TeamMember(UUID uuid) {
+            Validate.notNull(uuid);
+            this.uuid = uuid;
+        }
+
+        public TeamMember(PermissionUser user) {
+            Validate.notNull(user);
+
+            this.uuid = UUID.fromString(user.getIdentifier());
+            this.lastName = user.getName();
+        }
+
+        public UUID getUuid() {
+            return uuid;
+        }
+
+        /**
+         * @return An online state of this user, as found in the last online check.
+         */
+        public boolean isLastOnline() {
+            return lastOnline;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        /**
+         * Checks whether the given player is representing the same player as this object.
+         *
+         * @param plr PLayer to check
+         * @return This object's lastOnline state.
+         */
+        public boolean checkMatch(Player plr) {
+            lastOnline = (plr.getUniqueId().equals(getUuid()));
+            return lastOnline;
+        }
+
+        public String niceRepresentation() {
+            return MTCHelper.locArgs("XU-team" +
+                    (isLastOnline() ? "on" : "off") +
+                    "line", "CONSOLE", false, this.getLastName());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            TeamMember that = (TeamMember) o;
+
+            if (!uuid.equals(that.uuid)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            return uuid.hashCode();
+        }
     }
 
+    private static class TeamGroup {
+        private final Set<TeamMember> members = new HashSet<>();
+        private final String name;
+        private final String prefix;
+
+        public TeamGroup(String name, String prefix) {
+            Validate.notNull(name);
+            Validate.notNull(prefix);
+            this.name = name;
+            this.prefix = prefix;
+        }
+
+        /**
+         * Constructs a new TeamGroup from a PEx PermissionGroup.
+         * This will fetch name, prefix and members.
+         *
+         * @param permissionGroup Group to import
+         */
+        public TeamGroup(PermissionGroup permissionGroup) {
+            Validate.notNull(permissionGroup);
+
+            this.name = permissionGroup.getName();
+            this.prefix = permissionGroup.getPrefix();
+
+            this.members.addAll(permissionGroup.getUsers().parallelStream()
+                    .map(TeamMember::new)
+                    .collect(Collectors.toList()));
+        }
+
+        public Set<TeamMember> getMembers() {
+            return members;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPrefix() {
+            return prefix;
+        }
+
+        public String niceRepresentation() {
+            StringBuilder sb = new StringBuilder(this.prefix).append(' ');
+            String separator = MTCHelper.loc("XU-teamseperator", "CONSOLE", false);
+            this.members.parallelStream().forEach(member -> sb.append(member.niceRepresentation()).append(separator));
+            sb.delete(sb.length() - separator.length(), sb.length()); //Remove trailing separator
+            return sb.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            TeamGroup teamGroup = (TeamGroup) o;
+
+            if (!name.equals(teamGroup.name)) return false;
+            if (!prefix.equals(teamGroup.prefix)) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            result = 31 * result + prefix.hashCode();
+            return result;
+        }
+    }
 }
