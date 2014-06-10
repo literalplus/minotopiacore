eb5887a2f9fe59f8dcd8aca139b5c53f0e96f6d8
xxyy <devnull@nowak-at.net>
2014-06-11 01:54:14 +0200

    Maybe this will fix players not being displayed as online

--- src/main/java/io/github/xxyy/minotopiacore/misc/cmd/CommandTeam.java
+++ src/main/java/io/github/xxyy/minotopiacore/misc/cmd/CommandTeam.java
@@ -30,7 +30,7 @@ public class CommandTeam extends MTCCommandExecutor {
 
                 PermissionsEx.getPermissionManager().getGroupList().stream()
                         .filter(group -> group.getOptionBoolean("team", null, false))
-                        .sorted((group, group2) -> group.getOptionInteger("teamweight", null, 0) - group2.getOptionInteger("teamweight",null,0))
+                        .sorted((group, group2) -> group2.getOptionInteger("teamweight", null, 0) - group.getOptionInteger("teamweight",null,0))
                         .forEach(group -> this.groups.add(new TeamGroup(group)));
             } catch (Throwable e) {
                 sender.sendMessage("§cFehler beim Holen der Teammitglieder.");
@@ -87,7 +87,7 @@ public class CommandTeam extends MTCCommandExecutor {
          * @return This object's lastOnline state.
          */
         public boolean checkMatch(Player plr) {
-            lastOnline = (plr.getUniqueId().equals(getUuid()));
+            lastOnline = (plr.getUniqueId().equals(getUuid())) || plr.getName().equals(lastName); //TODO do we need the name check??
             return lastOnline;
         }
 
