a7acdf96ba857e189e6571933c7b93d73762cd34
xxyy <devnull@nowak-at.net>
2014-07-02 01:50:15 +0200

    Move notification format from SimpleCSF to MultiSubCSF

--- src/main/java/io/github/xxyy/minotopiacore/chat/cmdspy/MultiSubscriberCommandSpyFilter.java
+++ src/main/java/io/github/xxyy/minotopiacore/chat/cmdspy/MultiSubscriberCommandSpyFilter.java
@@ -9,14 +9,18 @@ import java.util.function.BiPredicate;
 import java.util.stream.Stream;
 
 /**
+ * A CommandSpyFilter impl that allows for multiple subscribers and keeps them internally as a set of their UUIDs.
+ *
  * @author <a href="http://xxyy.github.io/">xxyy</a>
  * @since 19.6.14
  */
 public class MultiSubscriberCommandSpyFilter extends SimpleCommandSpyFilter {
     private final Set<UUID> subscribers = new HashSet<>();
+    private final String notificationFormat;
 
     public MultiSubscriberCommandSpyFilter(String notificationFormat, BiPredicate<String, Player> predicate) {
-        super(notificationFormat, predicate);
+        super(predicate);
+        this.notificationFormat = notificationFormat;
     }
 
     public void notifySubscribers(String command, Player sender) {
@@ -53,5 +57,9 @@ public class MultiSubscriberCommandSpyFilter extends SimpleCommandSpyFilter {
             subscribers.remove(uuid);
         }
         return rtrn;
+    }
+
+    protected String getNotificationFormat() {
+        return notificationFormat;
     }
 }
