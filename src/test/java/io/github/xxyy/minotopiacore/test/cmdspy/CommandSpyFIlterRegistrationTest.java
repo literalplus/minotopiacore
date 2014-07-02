51a623eb4dda1c02e9df7d6c00ed46fa1dbe9312
xxyy <devnull@nowak-at.net>
2014-07-02 02:19:20 +0200

    Um I don't even get how that file could sneak out. It's the most important one.

---
+++ src/test/java/io/github/xxyy/minotopiacore/test/cmdspy/CommandSpyFIlterRegistrationTest.java
@@ -0,0 +1,91 @@
+package io.github.xxyy.minotopiacore.test.cmdspy;
+
+import com.google.common.collect.Lists;
+import io.github.xxyy.minotopiacore.chat.cmdspy.CommandSpyFilter;
+import io.github.xxyy.minotopiacore.chat.cmdspy.CommandSpyFilters;
+import io.github.xxyy.minotopiacore.test.TestHelper;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests the registration of CommandSpy filters.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 2.7.14
+ */
+public class CommandSpyFilterRegistrationTest {
+    private UUID targetId = UUID.randomUUID();
+    private UUID offlineId = UUID.randomUUID();
+    private Player[] playersWithTarget = new Player[]{TestHelper.mockPlayer(targetId, "test")};
+
+    @BeforeClass
+    public static void registerServer() {
+        Bukkit.setServer(TestHelper.mockServer());
+    }
+
+    @Test
+    public void removeDeadFiltersTest1() {
+        CommandSpyFilter filter = mock(CommandSpyFilter.class);
+        when(Bukkit.getServer().getOnlinePlayers()).thenReturn(new Player[0]);
+        when(filter.getSubscribers()).thenReturn(Lists.newArrayList());
+
+        CommandSpyFilters.registerFilter(filter);
+        Assert.assertTrue("Filter registration failed!", CommandSpyFilters.getActiveFilters().contains(filter));
+
+        CommandSpyFilters.removeDeadFilters();
+        Assert.assertFalse("Dead filter illegally persisted!", CommandSpyFilters.getActiveFilters().contains(filter));
+    }
+
+    @Test
+    public void removeDeadFiltersTest2() {
+        CommandSpyFilter filter = mock(CommandSpyFilter.class);
+        when(Bukkit.getServer().getOnlinePlayers()).thenReturn(playersWithTarget);
+        when(filter.getSubscribers()).thenReturn(Lists.newArrayList(targetId, offlineId));
+
+        CommandSpyFilters.registerFilter(filter);
+        Assert.assertTrue("Filter registration failed!", CommandSpyFilters.getActiveFilters().contains(filter));
+
+        CommandSpyFilters.removeDeadFilters();
+        Assert.assertTrue("Non-empty filter illegally removed!", CommandSpyFilters.getActiveFilters().contains(filter));
+
+        when(Bukkit.getServer().getOnlinePlayers()).thenReturn(new Player[0]);
+        CommandSpyFilters.removeDeadFilters();
+        Assert.assertFalse("Dead filter illegally persisted!", CommandSpyFilters.getActiveFilters().contains(filter));
+    }
+
+    @Test
+    public void removeOfflineSubscribersTest() {
+        CommandSpyFilter filter = mock(CommandSpyFilter.class);
+        when(Bukkit.getServer().getOnlinePlayers()).thenReturn(playersWithTarget);
+        when(filter.getSubscribers()).thenReturn(Lists.newArrayList(targetId, offlineId));
+
+        CommandSpyFilters.registerFilter(filter);
+        Assert.assertTrue("Filter registration failed!", CommandSpyFilters.getActiveFilters().contains(filter));
+
+        CommandSpyFilters.removeOfflineSubscribers(filter);
+        Assert.assertTrue("Online UUID illegally removed!", filter.getSubscribers().contains(targetId));
+        Assert.assertFalse("Offline UUID illegally persisted!", filter.getSubscribers().contains(offlineId));
+    }
+
+    @Test
+    public void getSubscribedFiltersTest() {
+        CommandSpyFilter filter = mock(CommandSpyFilter.class);
+        when(Bukkit.getServer().getOnlinePlayers()).thenReturn(playersWithTarget);
+        when(filter.getSubscribers()).thenReturn(Lists.newArrayList(targetId, offlineId));
+
+        CommandSpyFilters.registerFilter(filter);
+        Assert.assertTrue("Filter registration failed!", CommandSpyFilters.getActiveFilters().contains(filter));
+
+        CommandSpyFilters.removeOfflineSubscribers(filter);
+        Assert.assertTrue("Online UUID illegally removed!", filter.getSubscribers().contains(targetId));
+        Assert.assertFalse("Offline UUID illegally persisted!", filter.getSubscribers().contains(offlineId));
+    }
+}
