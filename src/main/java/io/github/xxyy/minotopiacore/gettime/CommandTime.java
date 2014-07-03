041804f8ee5b242824f76d89f68921e092b8abe6
xxyy <devnull@nowak-at.net>
2014-07-03 18:11:25 +0200

    Refactor CommandTime. Fixes #136

--- src/main/java/io/github/xxyy/minotopiacore/gettime/CommandTime.java
+++ src/main/java/io/github/xxyy/minotopiacore/gettime/CommandTime.java
@@ -11,30 +11,28 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 
-public final class CommandTime implements CommandExecutor{
+public final class CommandTime implements CommandExecutor {
 
-	@Override
-	//onCommand(sender,command,label(Comamnd that was typed by user),args
-	//reacts to command
-	public boolean onCommand(CommandSender sender, Command cmd, String label,String[] args) {
-		if(!MTCHelper.isEnabledAndMsg(".command.gtime", sender)) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!MTCHelper.isEnabledAndMsg(".command.gtime", sender)) {
             return true;
         }
-		if(!CommandHelper.checkPermAndMsg(sender, "mtc.cmd.gtime", label)) {
+        if (!CommandHelper.checkPermAndMsg(sender, "mtc.cmd.gtime", label)) {
             return true;
         }
-		if(args.length != 0){
-			sender.sendMessage("§7[MTS][Info]Dieser Befehl ben§tigt keine Argumente!");
-		}
-		sender.sendMessage(ChatColor.GOLD + "============={§7Serverzeit"+ChatColor.RESET+ChatColor.GOLD+"}=============");
-		if(CommandHelper.checkActionPermAndMsg(sender, "mtc.cmd.gtime.time", "Die RL-Serverzeit anzeigen")) {
+        if (args.length != 0) {
+            sender.sendMessage("§7[MTS][Info]Dieser Befehl ben§tigt keine Argumente!");
+        }
+        sender.sendMessage(ChatColor.GOLD + "============={§7Serverzeit" + ChatColor.RESET + ChatColor.GOLD + "}=============");
+        if (CommandHelper.checkActionPermAndMsg(sender, "mtc.cmd.gtime.time", "Die RL-Serverzeit anzeigen")) {
             sender.sendMessage(ChatColor.GOLD + "Aktuelle RL-Serverzeit: " + ChatColor.BLUE + (new SimpleDateFormat("HH:mm:ss")).format(Calendar.getInstance().getTime()));
         }
-		if(!label.equalsIgnoreCase("gd") && !label.equalsIgnoreCase("getdate") && CommandHelper.checkActionPermAndMsg(sender, "mtc.cmd.gtime.date", "Das Serverdatum anzeigen")) {
+        if (!label.equalsIgnoreCase("gd") && !label.equalsIgnoreCase("getdate") && CommandHelper.checkActionPermAndMsg(sender, "mtc.cmd.gtime.date", "Das Serverdatum anzeigen")) {
             sender.sendMessage(ChatColor.GOLD + "Aktuelles Serverdatum: " + ChatColor.BLUE + (new SimpleDateFormat("dd.MM.yyyy")).format(Calendar.getInstance().getTime()));
         }
-		sender.sendMessage(ChatColor.GOLD + "============={§7Serverzeit"+ChatColor.RESET+ChatColor.GOLD+"}=============");
-		return true;
-	}
-	
+        sender.sendMessage(ChatColor.GOLD + "============={§7Serverzeit" + ChatColor.RESET + ChatColor.GOLD + "}=============");
+        return true;
+    }
+
 }
