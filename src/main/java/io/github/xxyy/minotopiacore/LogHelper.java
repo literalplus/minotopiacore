05074b520dd20011d81ad075faae5a11e05091b6
xxyy <devnull@nowak-at.net>
2014-07-20 18:24:59 +0200

    Fix compiler error from 1ce38f, Add more checks & Java 8 streams to that code

--- src/main/java/io/github/xxyy/minotopiacore/LogHelper.java
+++ src/main/java/io/github/xxyy/minotopiacore/LogHelper.java
@@ -4,6 +4,7 @@ import io.github.xxyy.common.log.XYCFormatter;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -25,7 +26,7 @@ public final class LogHelper {
 
     }
 
-    public static void flushAll(){
+    public static void flushAll() {
         LogHelper.flush(LogHelper.MAIN_LOGGER);
         LogHelper.flush(LogHelper.BAD_CMD_LOGGER);
         LogHelper.flush(LogHelper.CHAT_LOGGER);
@@ -67,22 +68,33 @@ public final class LogHelper {
     public static Logger getWarnLogger() {
         return LogHelper.WARN_LOGGER;
     }
-    
-    public static void initLogs(){
 
-        File file = new File(MTC.instance().getDataFolder()+"/logs/").mkdirs();
-        for (File a : file.listFiles()) {
-            if(a.getName().endsWith(".lck")) {
-                a.delete();
+    public static void initLogs() {
+
+        File logDirectory = new File(MTC.instance().getDataFolder() + "/logs/");
+
+        if (!logDirectory.isDirectory()) {
+            if(logDirectory.isFile()) {
+                throw new IllegalStateException("logs in the plugin data dir is a file while it should be a directory."); //The day this happens is the day I send every user a package with a cookie
+
             }
+            if (!logDirectory.mkdirs()) {
+                throw new IllegalStateException("Couldn't create logs directory.");
+            }
+        } else {
+            //noinspection ConstantConditions
+            Arrays.asList(logDirectory.listFiles()).stream() //If this returns null we're fucked anyways
+                    .filter(fl -> fl.getName().contains(".lck")) //.lck.0
+                    .forEach(File::delete);
         }
-        if(ConfigHelper.isMainLoggerEnabled()){
+
+        if (ConfigHelper.isMainLoggerEnabled()) {
             try {
-                FileHandler hdlr = new FileHandler(MTC.instance().getDataFolder()+"/logs/main.log",10000000, 5, true);
+                FileHandler hdlr = new FileHandler(MTC.instance().getDataFolder() + "/logs/main.log", 10000000, 5, true);
                 hdlr.setLevel(Level.FINEST);
                 hdlr.setFormatter(new XYCFormatter(MTC.instance(), "Main Log.", true));
                 hdlr.setEncoding("UTF-8");
-                FileHandler hdlr2 = new FileHandler(MTC.instance().getDataFolder()+"/logs/error.log",10000000, 5, true);
+                FileHandler hdlr2 = new FileHandler(MTC.instance().getDataFolder() + "/logs/error.log", 10000000, 5, true);
                 hdlr2.setLevel(Level.WARNING);
                 hdlr2.setFormatter(new XYCFormatter(MTC.instance(), "Main Log.", true));
                 hdlr2.setEncoding("UTF-8");
@@ -95,20 +107,20 @@ public final class LogHelper {
                 System.out.println(">>MTC exception when tryin to initialize cmd loggerz.");
             }
         }
-        if(MTC.instance().getConfig().getBoolean("enable.log.cmds",false)){
-            LogHelper.tryInitLogger(LogHelper.BAD_CMD_LOGGER,MTC.instance().getDataFolder()+"/logs/badcmds.log","BadCmdLogger",true);
+        if (MTC.instance().getConfig().getBoolean("enable.log.cmds", false)) {
+            LogHelper.tryInitLogger(LogHelper.BAD_CMD_LOGGER, MTC.instance().getDataFolder() + "/logs/badcmds.log", "BadCmdLogger", true);
         }
-        if(MTC.instance().getConfig().getBoolean("enable.log.chat",true)){
-            LogHelper.tryInitLogger(LogHelper.CHAT_LOGGER,MTC.instance().getDataFolder()+"/logs/chat.log","ChatLogger",true);
-            LogHelper.tryInitLogger(LogHelper.CLAN_CHAT_LOGGER,MTC.instance().getDataFolder()+"/logs/clanchat.log","ClanChatLogger",true);
-            LogHelper.tryInitLogger(LogHelper.PRIV_CHAT_LOGGER,MTC.instance().getDataFolder()+"/logs/privatechats.log","PrivChatLogger",true);
+        if (MTC.instance().getConfig().getBoolean("enable.log.chat", true)) {
+            LogHelper.tryInitLogger(LogHelper.CHAT_LOGGER, MTC.instance().getDataFolder() + "/logs/chat.log", "ChatLogger", true);
+            LogHelper.tryInitLogger(LogHelper.CLAN_CHAT_LOGGER, MTC.instance().getDataFolder() + "/logs/clanchat.log", "ClanChatLogger", true);
+            LogHelper.tryInitLogger(LogHelper.PRIV_CHAT_LOGGER, MTC.instance().getDataFolder() + "/logs/privatechats.log", "PrivChatLogger", true);
         }
-        if(MTC.instance().getConfig().getBoolean("enable.log.bansNwarns",true)){
-            LogHelper.tryInitLogger(LogHelper.WARN_LOGGER,MTC.instance().getDataFolder()+"/logs/warns.log","WarnLogger",true);
-            LogHelper.tryInitLogger(LogHelper.BAN_LOGGER,MTC.instance().getDataFolder()+"/logs/bans.log","BanLogger",true);
+        if (MTC.instance().getConfig().getBoolean("enable.log.bansNwarns", true)) {
+            LogHelper.tryInitLogger(LogHelper.WARN_LOGGER, MTC.instance().getDataFolder() + "/logs/warns.log", "WarnLogger", true);
+            LogHelper.tryInitLogger(LogHelper.BAN_LOGGER, MTC.instance().getDataFolder() + "/logs/bans.log", "BanLogger", true);
         }
-        if(ConfigHelper.isFullLogEnabled()){
-            LogHelper.tryInitLogger(LogHelper.FULL_LOGGER, MTC.instance().getDataFolder()+"/logs/fulls.log","FullLogger",true);
+        if (ConfigHelper.isFullLogEnabled()) {
+            LogHelper.tryInitLogger(LogHelper.FULL_LOGGER, MTC.instance().getDataFolder() + "/logs/fulls.log", "FullLogger", true);
         }
         LogHelper.MAIN_LOGGER.setUseParentHandlers(false);
         LogHelper.BAD_CMD_LOGGER.setUseParentHandlers(false);
@@ -119,14 +131,14 @@ public final class LogHelper {
         LogHelper.WARN_LOGGER.setUseParentHandlers(false);
         LogHelper.FULL_LOGGER.setUseParentHandlers(false);
     }
-    
-    private static void flush(Logger lgr){
-        for(Handler hdlr : lgr.getHandlers()){
+
+    private static void flush(Logger lgr) {
+        for (Handler hdlr : lgr.getHandlers()) {
             hdlr.flush();
         }
     }
-    
-    private static void initLogger(Logger lgr, String fileName, String loggerName, boolean setParent) throws Exception{
+
+    private static void initLogger(Logger lgr, String fileName, String loggerName, boolean setParent) throws Exception {
         FileHandler hdlr = new FileHandler(fileName);
         hdlr.setLevel(Level.FINEST);
         hdlr.setFormatter(new XYCFormatter(MTC.instance(), loggerName, false));
@@ -134,17 +146,17 @@ public final class LogHelper {
 //        lgr.removeHandler(lgr.getHandlers()[0]);
         lgr.addHandler(hdlr);
         lgr.setLevel(Level.FINEST);
-        if(setParent) {
+        if (setParent) {
             lgr.setParent(LogHelper.MAIN_LOGGER);
         }
     }
-    
-    private static void tryInitLogger(Logger lgr, String fileName, String loggerName, boolean setParent){
-        try{
-            LogHelper.initLogger(lgr,fileName, loggerName, setParent);
-        }catch(Exception e){
+
+    private static void tryInitLogger(Logger lgr, String fileName, String loggerName, boolean setParent) {
+        try {
+            LogHelper.initLogger(lgr, fileName, loggerName, setParent);
+        } catch (Exception e) {
             e.printStackTrace();
-            System.out.println(">>MTC exception when tryin to initialize "+loggerName+".");
+            System.out.println(">>MTC exception when tryin to initialize " + loggerName + ".");
         }
     }
 }
