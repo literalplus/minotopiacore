94f437b6d2aec35d99fae1ad727c59a43c6859bc
xxyy <devnull@nowak-at.net>
2014-10-11 02:50:32 +0200

    Add a config option to WebsiteModule to control whether players can change passwords directly in-game; #45

--- common/src/main/java/io/github/xxyy/mtc/module/website/WebsiteModule.java
+++ common/src/main/java/io/github/xxyy/mtc/module/website/WebsiteModule.java
@@ -4,7 +4,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 
 import io.github.xxyy.mtc.MTC;
-import io.github.xxyy.mtc.module.MTCModuleAdapter;
+import io.github.xxyy.mtc.module.ConfigurableMTCModule;
 
 /**
  * <p>
@@ -17,14 +17,17 @@ import io.github.xxyy.mtc.module.MTCModuleAdapter;
  * @author <a href="http://xxyy.github.io/">xxyy</a>
  * @since 10/10/14
  */
-public final class WebsiteModule extends MTCModuleAdapter implements Listener {
-    public static final String ONLINE_TABLE_NAME = "ni176987_1_DB.onlineuser"; //lagacy name, not my choice
+public final class WebsiteModule extends ConfigurableMTCModule implements Listener {
+    public static final String ONLINE_TABLE_NAME = "ni176987_1_DB.onlineuser"; //legacy name, not my choice
     public static final String PLAYTIME_TABLE_NAME = "mt_homepage.play_time";
+    public static final String WEBSITE_USER_TABLE_NAME = "ni176987_1_DB.hp_user"; //legacy name
     public static final String NAME = "Website";
+    private static final String PASSWORD_CHANGING_PATH = "password-changing";
+    private boolean passwordChangeEnabled = true;
     private WebsiteListener listener;
 
     public WebsiteModule() {
-        super(NAME);
+        super(NAME, "website_config.yml", ClearCacheBehaviour.RELOAD);
     }
 
     @Override
@@ -36,7 +39,19 @@ public final class WebsiteModule extends MTCModuleAdapter implements Listener {
     }
 
     @Override
+    protected void reloadImpl() {
+        configuration.addDefault(PASSWORD_CHANGING_PATH, true);
+        configuration.options().copyDefaults(true);
+
+        passwordChangeEnabled = configuration.getBoolean(PASSWORD_CHANGING_PATH);
+    }
+
+    @Override
     public void disable(MTC plugin) {
         HandlerList.unregisterAll(listener); //We'll get some reload functionality working for modules eventually
+    }
+
+    public boolean isPasswordChangeEnabled() {
+        return passwordChangeEnabled;
     }
 }
