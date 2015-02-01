literalplus/minotopiacore

commit a8d1fcf502bae7f6d089359575172096750d9774

Improve module API
This makes loading modules by class possible, which is useful because it can also easily catch exceptions thrown from constructors. This resolves one module erroring making the whole plugin unusable. Additionally, it allows for shorter loading code and is the base for a possible future annotation-based loading facility.

common/src/main/java/io/github/xxyy/mtc/module/MTCModuleAdapter.java
@@ -11,13 +11,22 @@ import com.google.common.collect.ImmutableList;
 
 import io.github.xxyy.mtc.MTC;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
- * Abstract base class for MTC modules.
+ * Abstract base class for MTC modules. Note when implementing that the Reflection-based loading facility expects your
+ * class to have a constructor with no arguments. If you don't provide such, your module won't be able to be loaded
+ * automatically.
  *
  * @author <a href="http://xxyy.github.io/">xxyy</a>
  * @since 22.8.14
@@ -78,5 +87,50 @@ public abstract class MTCModuleAdapter implements MTCModule {
 
     public static void forEach(Consumer<MTCModule> consumer) {
         instances.values().forEach(consumer);
+    }
+
+    /**
+     * Attempts to load MTC module from a list of classes. If any of the classes do not implement {@link io.github.xxyy.mtc.module.MTCModule},
+     * they will not be loaded and an error message will be logged to {@code plugin}'s logger.
+     * @param plugin the plugin requesting this load operation
+     * @param moduleClasses the classes to be loaded
+     * @return a list of all created instances
+     * @see #load(io.github.xxyy.mtc.MTC, Class)
+     */
+    public static List<MTCModule> load(MTC plugin, Class... moduleClasses) {
+        //noinspection unchecked
+        return Stream.of(moduleClasses)
+                .map((c) -> load(plugin, c)) //This method checks if the class implements MTCModule and returns null otherwise, logging an error.
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Attempts to instantiate and load a MTC module, logging an error message to {@code plugin}'s logger if any error occurs.
+     * @param plugin the plugin requesting this operation
+     * @param moduleClass the class of the module to load
+     * @return the module instance created, {@code null} otherwise.
+     */
+    public static MTCModule load(MTC plugin, Class<? extends MTCModule> moduleClass) {
+        if (!MTCModule.class.isAssignableFrom(moduleClass)) {
+            plugin.getLogger().severe("**** Unable to load " + moduleClass.getName() + " - Does not implement MTCModule!");
+            return null;
+        }
+
+        try {
+            Constructor<? extends MTCModule> constructor = moduleClass.getConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException e) {
+            plugin.getLogger().severe("**** Unable to load " + moduleClass.getName() + " - Modules must specify a default constructor!");
+        } catch (InvocationTargetException e) {
+            plugin.getLogger().log(Level.SEVERE, "**** Unable to load " + moduleClass.getName() + " - Exception thrown from constructor: ", e.getCause());
+        } catch (InstantiationException e) {
+            plugin.getLogger().log(Level.SEVERE, "**** Unable to load " + moduleClass.getName() + " - Cannot instantiate abstract classes: ", e.getCause());
+        } catch (Exception e) {
+            plugin.getLogger().log(Level.SEVERE, "**** Unable to load" + moduleClass.getName() + " - Unhandled Exception: ", e);
+        }
+
+        return null;
     }
 }

common/src/main/java/io/github/xxyy/mtc/ConfigHelper.java
@@ -158,7 +158,6 @@ public class ConfigHelper {
         ConfigHelper.addClanDefaults(cfg);
         ConfigHelper.addFixDefaults(cfg);
         ConfigHelper.addMiscDefaults(cfg);
-        MTC.instance().saveConfig();
 
         ConfigHelper.initClassProperties(cfg);
     }

common/src/main/java/io/github/xxyy/mtc/MTC.java
@@ -173,6 +173,7 @@ public class MTC extends SqlXyPlugin implements XyLocalizable {
 
         //MODULES
         loadModules();
+        saveConfig(); //Save here so that changes from modules also apply to the config file
 
         //HELP
         MTCHelper.initHelp();
@@ -259,12 +260,8 @@ public class MTC extends SqlXyPlugin implements XyLocalizable {
     }
 
     private void loadModules() { //TODO: proper, configurable, maybe even annotation-based loading thing
-        new InfiniteDispenserModule();
-        new TrueFalseModule();
-        new WebsiteModule();
-        new QuizModule();
-        new RepeaterModule();
-        new ChalModule();
+        MTCModuleAdapter.load(this, InfiniteDispenserModule.class, TrueFalseModule.class, WebsiteModule.class,
+                QuizModule.class, RepeaterModule.class, ChalModule.class);
     }
 
     private void registerCommands() {

commit 37c7f187b4029a9a9fa4e6d88b8564d925cc95ba

Fix compiler error introduced in a8d1fcf

common/src/main/java/io/github/xxyy/mtc/module/MTCModuleAdapter.java
@@ -99,10 +99,10 @@ public abstract class MTCModuleAdapter implements MTCModule {
      */
     public static List<MTCModule> load(MTC plugin, Class... moduleClasses) {
         //noinspection unchecked
-        return Stream.of(moduleClasses)
+        return ((List<MTCModule>) Stream.of(moduleClasses) //Compiler won't accept without cast for some reason
                 .map((c) -> load(plugin, c)) //This method checks if the class implements MTCModule and returns null otherwise, logging an error.
                 .filter(Objects::nonNull)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
     /**