7804116d6f39c10fa9fb4195fbc65d39737e06ee
xxyy <devnull@nowak-at.net>
2015-02-06 01:12:53 +0100

    Add JavaDocs to MTCModule and add throws Exception to enable() for convenience (cherry picked from commit 16a6a54)

--- common/src/main/java/io/github/xxyy/mtc/module/MTCModule.java
+++ common/src/main/java/io/github/xxyy/mtc/module/MTCModule.java
@@ -17,10 +17,39 @@ import io.github.xxyy.mtc.misc.CacheHelper;
  * @since 22.8.14
  */
 public interface MTCModule extends CacheHelper.Cache {
-    void enable(MTC plugin);
+    /**
+     * Enables this module.
+     *
+     * @param plugin the plugin managing your module today
+     * @throws Exception thrown if anything fails, meaning the module did not get enabled
+     */
+    void enable(MTC plugin) throws Exception;
+
+    /**
+     * Disables this module. This does not allow exceptions since plugins should disable when asked and not be able to
+     * put requirements.
+     *
+     * @param plugin the plugin managing your module, again
+     */
     void disable(MTC plugin);
+
+    /**
+     * Reloads this module's configurations and related facilities.
+     *
+     * @param plugin the plugin issuing the reload
+     */
     void reload(MTC plugin);
 
+    /**
+     * @return a hopefully unique and definitely static name describing this module
+     */
     String getName();
+
+    /**
+     * Checks whether this plugin is generally ready to be enabled. This checks a value in the global config by default.
+     *
+     * @param plugin the plugin requesting the state
+     * @return whether this plugin wants to be enabled (modules have feelings, too)
+     */
     boolean isEnabled(MTC plugin);
 }
