4e1f483c61eaa78573776a5d583c7b20f5d1b603
Janmm14 <computerjanimaus@yahoo.de>
2015-03-01 00:14:16 +0100

    rework InfiniteDispenserModule to constantly allow any block that is an InventoryHolder and added infinite anvils support (clicked anvils will be renewed)

--- common/src/main/java/io/github/xxyy/mtc/module/InfiniteBlockModule.java
+++ common/src/main/java/io/github/xxyy/mtc/module/InfiniteBlockModule.java
@@ -7,6 +7,12 @@
 
 package io.github.xxyy.mtc.module;
 
+import io.github.xxyy.common.misc.XyLocation;
+import io.github.xxyy.common.util.CommandHelper;
+import io.github.xxyy.mtc.MTC;
+import io.github.xxyy.mtc.helper.MTCHelper;
+import io.github.xxyy.mtc.misc.ClearCacheBehaviour;
+import io.github.xxyy.mtc.misc.cmd.MTCCommandExecutor;
 import mkremins.fanciful.FancyMessage;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -18,20 +24,16 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.AnvilInventory;
 import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
-import org.bukkit.metadata.MetadataValueAdapter;
-
-import io.github.xxyy.common.misc.XyLocation;
-import io.github.xxyy.common.util.CommandHelper;
-import io.github.xxyy.mtc.MTC;
-import io.github.xxyy.mtc.helper.MTCHelper;
-import io.github.xxyy.mtc.misc.ClearCacheBehaviour;
-import io.github.xxyy.mtc.misc.cmd.MTCCommandExecutor;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -39,13 +41,13 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
-public final class InfiniteDispenserModule extends ConfigurableMTCModule implements Listener {
-    public static final String NAME = "InfiniteDispensers";
+public final class InfiniteBlockModule extends ConfigurableMTCModule implements Listener {
+    public static final String NAME = "InfiniteDispensers"; // don't change such constants as that would require manual cfg changes
     public static final String INFINITY_TAG = "mtc.infinite";
     private static final String DATA_PATH = "dispensers";
-    private List<XyLocation> dispenserLocations;
+    private List<XyLocation> infiniteBlockLocations;
 
-    public InfiniteDispenserModule() {
+    public InfiniteBlockModule() {
         super(NAME, "infdisps.stor.yml", ClearCacheBehaviour.SAVE);
     }
 
@@ -60,18 +62,19 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
     @SuppressWarnings("unchecked")
     @Override
     protected void reloadImpl() {
-        dispenserLocations = (List<XyLocation>) configuration.getList(DATA_PATH, new ArrayList<XyLocation>());
+        infiniteBlockLocations = (List<XyLocation>) configuration.getList(DATA_PATH, new ArrayList<XyLocation>());
 
-        Iterator<XyLocation> it = dispenserLocations.iterator();
+        Iterator<XyLocation> it = infiniteBlockLocations.iterator();
 
-        while (it.hasNext()) {
+        while (it.hasNext()) { // any reason not using enhanced for loop or #forEach?
             XyLocation location = it.next();
-            BlockState blockState = location.getBlock().getState();
-            if (blockState.getType() != Material.HOPPER && blockState.getType() != Material.DISPENSER && blockState.getType() != Material.DROPPER) {
-                plugin.getLogger().info("Removing infinite dispenser at " + location.pretyPrint() + " because the block changed to " + blockState.getType() + "!");
+            Block blk = location.getBlock();
+            BlockState state = blk.getState();
+            if (!canBeMadeInfinite(state)) {
+                plugin.getLogger().info("Removing infinite tag at " + location.pretyPrint() + " because the block changed to " + state.getType() + "!");
                 it.remove();
             } else {
-                initDispenser(blockState);
+                addInfiniteMetadata(state);
             }
         }
 
@@ -83,7 +86,7 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
 
     }
 
-    protected void addDispenser(Location input) {
+    public void addInfiniteToCfg(Location input) { //REFACTOR: un-spaghetti //will be changed separately if xyc is updated
         XyLocation loc;
         if (input instanceof XyLocation) {
             loc = (XyLocation) input;
@@ -91,12 +94,12 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
             loc = new XyLocation(input);
         }
 
-        dispenserLocations.add(loc);
-        configuration.set(DATA_PATH, dispenserLocations);
+        infiniteBlockLocations.add(loc);
+        configuration.set(DATA_PATH, infiniteBlockLocations);
         save();
     }
 
-    protected void removeDispenser(Location input) { //REFACTOR: un-spaghetti
+    public void removeInfiniteFromCfg(Location input) { //REFACTOR: un-spaghetti //will be changed separately if xyc is updated
         XyLocation loc;
         if (input instanceof XyLocation) {
             loc = (XyLocation) input;
@@ -104,32 +107,30 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
             loc = new XyLocation(input);
         }
 
-        dispenserLocations.remove(loc);
-        configuration.set(DATA_PATH, dispenserLocations);
+        infiniteBlockLocations.remove(loc);
+        configuration.set(DATA_PATH, infiniteBlockLocations);
         save();
     }
 
-    protected void initDispenser(BlockState dispenser) {
-        dispenser.setMetadata(INFINITY_TAG, new MetadataValueAdapter(plugin) {
-            @Override
-            public void invalidate() {
-            }
-
-            @Override
-            public Object value() {
-                return true;
-            }
-        });
+    private void addInfiniteMetadata(BlockState dispenser) {
+        dispenser.setMetadata(INFINITY_TAG, new FixedMetadataValue(plugin, null));
     }
 
-    protected void doIfInfinite(InventoryHolder possibleState, Consumer<MetadataValue> consumer) {
+    private void doIfInfinite(InventoryHolder possibleState, Consumer<MetadataValue> consumer) {
         if (possibleState instanceof BlockState) {
             doIfInfinite((BlockState) possibleState, consumer);
         }
     }
 
-    protected void doIfInfinite(BlockState state, Consumer<MetadataValue> consumer) {
+    private void doIfInfinite(BlockState state, Consumer<MetadataValue> consumer) {
         state.getMetadata(INFINITY_TAG).stream() //If performance issues arise, make this an isEmpty() call - https://twitter.com/_xxyy/status/556618846401216512
+                .filter(val -> plugin.equals(val.getOwningPlugin()))
+                .limit(1)
+                .forEach(consumer);
+    }
+
+    private void doIfInfinite(Block blk, Consumer<MetadataValue> consumer) {
+        blk.getMetadata(INFINITY_TAG).stream() //If performance issues arise, make this an isEmpty() call - https://twitter.com/_xxyy/status/556618846401216512
                 .filter(val -> plugin.equals(val.getOwningPlugin()))
                 .limit(1)
                 .forEach(consumer);
@@ -138,9 +139,9 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
     ////////////// EVENT HANDLERS //////////////////////////////////////////////////////////////////////////////////////
 
     @EventHandler(ignoreCancelled = true)
-    public void onInfiniteDispenser(BlockDispenseEvent evt) {
-        BlockState dispenser = evt.getBlock().getState();
-        doIfInfinite(dispenser, val -> ((InventoryHolder) dispenser).getInventory().addItem(evt.getItem().clone()));
+    public void onInfiniteDispenserOrDropper(BlockDispenseEvent evt) {
+        BlockState state = evt.getBlock().getState();
+        doIfInfinite(state, val -> ((InventoryHolder) state).getInventory().addItem(evt.getItem().clone()));
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -151,18 +152,44 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
 
     @EventHandler(ignoreCancelled = true)
     public void onInfiniteInventoryOpen(InventoryOpenEvent evt) {
-        doIfInfinite(evt.getInventory().getHolder(), val -> {
-            evt.setCancelled(true);
-            MTCHelper.sendLoc("XU-infdispclk", (Player) evt.getPlayer(), true);
-        });
+        if (!(evt.getInventory() instanceof AnvilInventory)) {
+            doIfInfinite(evt.getInventory().getHolder(), val -> {
+                evt.setCancelled(true);
+                MTCHelper.sendLoc("XU-infdispclk", (Player) evt.getPlayer(), true);
+            });
+        }
     }
 
     @EventHandler(ignoreCancelled = true)
-    public void onInvPickup(InventoryPickupItemEvent evt) {
+    public void onInventoryPickup(InventoryPickupItemEvent evt) {
         doIfInfinite(evt.getInventory().getHolder(), val -> {
             evt.setCancelled(true);
             evt.getItem().remove();
         });
+    }
+
+    @EventHandler
+    @SuppressWarnings("deprecation")
+    public void onAnvilClick(PlayerInteractEvent evt) {
+        if (evt.hasBlock() && evt.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            Block blk = evt.getClickedBlock();
+            if (blk.getType() == Material.ANVIL) {   //always the byte casts //don't update physics so anvil can't fall down when clicked if placed w/o physics on
+                doIfInfinite(blk, val -> blk.setData(getUndamagedDataValueOfAnvil(blk.getData()), false)); //no possibility without using a deprecated method
+            }
+        }
+    }
+
+    private byte getUndamagedDataValueOfAnvil(byte dataValue) { // see http://minecraft.gamepedia.com/Anvil#Data_values "Block"
+        if (dataValue >= 4) {
+            if (dataValue < 8) {
+                dataValue -= 4;
+            } else if (dataValue < 12) {
+                return dataValue -= 8;
+            } else {
+                throw new IllegalArgumentException("invalid anvil block data value");
+            }
+        }
+        return dataValue;
     }
 
     /////////////////////////// COMMAND HANDLER ////////////////////////////////////////////////////////////////////////
@@ -185,8 +212,8 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
                         if (disp == null) {
                             return true;
                         }
-                        addDispenser(disp.getLocation());
-                        initDispenser(disp);
+                        addInfiniteToCfg(disp.getLocation());
+                        addInfiniteMetadata(disp);
                         return MTCHelper.sendLoc("XU-infdispon", sender, true);
                     case "off":
                         BlockState disp2 = checkTargetBlock(plr);
@@ -194,11 +221,11 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
                             return true;
                         }
                         disp2.removeMetadata(INFINITY_TAG, plugin);
-                        removeDispenser(disp2.getLocation());
+                        removeInfiniteFromCfg(disp2.getLocation());
                         return MTCHelper.sendLoc("XU-infdispoff", sender, true);
                     case "list":
                         AtomicInteger i = new AtomicInteger(0);
-                        dispenserLocations.stream().forEach(loc -> {
+                        infiniteBlockLocations.stream().forEach(loc -> {
                             //@formatter:off
                             new FancyMessage(loc.getBlock().getType() + " @ ")
                                         .color(ChatColor.GOLD)
@@ -211,6 +238,7 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
                             //@formatter:on
                         });
                         sender.sendMessage("§6" + i.get() + " §eInfiniteDispenser registriert.");
+                        //intended fall-through?
                     default:
                         break;
                 }
@@ -221,11 +249,23 @@ public final class InfiniteDispenserModule extends ConfigurableMTCModule impleme
         private BlockState checkTargetBlock(Player plr) {
             @SuppressWarnings("deprecation")
             Block blk = plr.getTargetBlock(null, 100); //BUKKIT! HAVEN'T I TOLD YOU NOT TO DEPRECATED USEFUL STUFF?!
-            if (blk == null || !(blk.getState() instanceof InventoryHolder)) {
+            if (blk == null) {
+                MTCHelper.sendLoc("XU-nodisp", plr, true);
+                return null;
+            } else {
+                BlockState state = blk.getState(); //just call Block#getState() once, as it consumes time
+
+                if (canBeMadeInfinite(state)) {
+                    return state;
+                }
                 MTCHelper.sendLoc("XU-nodisp", plr, true);
                 return null;
             }
-            return blk.getState();
         }
     }
+    private boolean canBeMadeInfinite(BlockState state) {
+        //maybe use a chain of material checks instead of computing the state of block even if it does not fit to the criteria
+        return state.getType() == Material.ANVIL || state instanceof InventoryHolder;
+    }
+
 }
