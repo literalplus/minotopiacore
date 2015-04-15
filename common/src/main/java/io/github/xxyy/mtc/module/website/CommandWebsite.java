8a670baf5a0dd4c749c9e69538763f8e7c5792d5
xxyy <devnull@nowak-at.net>
2015-04-16 00:41:30 +0200

    Add warning message to /hp activate, Bump XYC dep version
    Some users began to ask others to execute this command for them after registering an account on the website with their name. The other would not know what the command would do and gladly accept some arbitrary reward. The hacker would now have access to website function in their name, including money transfers. Using that, they could transfer any arbitrary sum of in-game money from the victim's account to their own one. If they repeated this process, they could gain a lot of money and also dissatisfy their victims, possibly causing them to leave the server forever. While we have not yet been able to resolve this problem completely so that hackers cannot ask victims to execute a simple command in order to gain access to their website account, we have added a red and bold warning message informing the possible victim of the possible danger. While this may scare new users, it should resolve the problem outlined above for users with at least average intelligence.

--- common/src/main/java/io/github/xxyy/mtc/module/website/CommandWebsite.java
+++ common/src/main/java/io/github/xxyy/mtc/module/website/CommandWebsite.java
@@ -7,14 +7,21 @@
 
 package io.github.xxyy.mtc.module.website;
 
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 
+import io.github.xxyy.common.util.StringHelper;
 import io.github.xxyy.common.util.math.NumberHelper;
 import io.github.xxyy.mtc.misc.cmd.MTCPlayerOnlyCommandExecutor;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Handles /website command, which can be used to enable and disable the player's website account as well as change the
@@ -25,6 +32,8 @@ import java.security.NoSuchAlgorithmException;
  */
 public class CommandWebsite extends MTCPlayerOnlyCommandExecutor {
     private final WebsiteModule module;
+    //Player UUIDs to confirmation codes used for /hp (de)?activate
+    private final Map<UUID, String> playerCodes = new HashMap<>();
 
     public CommandWebsite(WebsiteModule module) {
         this.module = module;
@@ -38,11 +47,13 @@ public class CommandWebsite extends MTCPlayerOnlyCommandExecutor {
                 case "activate":
                 case "activ8":
                 case "aktivieren!": //For the special snowflakes who don't get that they should not include the exclamation mark when it says "use /hp aktivieren!"
-                    setAccountStatus(plr, "aktiviert! Du kannst dich jetzt einloggen!", true); //activated
+                    if (hasConfirmedActivate(plr, args)) { //also prints a red warning message in bold ok
+                        setAccountStatus(plr, "aktiviert! Du kannst dich jetzt einloggen!", true, args); //activated
+                    }
                     return true;
                 case "deaktivieren":
                 case "deactivate":
-                    setAccountStatus(plr, "§cde§aaktiviert!", false); //deactivated
+                    setAccountStatus(plr, "§cde§aaktiviert!", false, args); //deactivated
                     return true;
                 case "pw":
                     return changePassword(plr, label, args);
@@ -58,7 +69,7 @@ public class CommandWebsite extends MTCPlayerOnlyCommandExecutor {
     }
 
     private boolean changePassword(Player plr, String label, String[] args) {
-        if(!module.isPasswordChangeEnabled()) {
+        if (!module.isPasswordChangeEnabled()) {
             plr.sendMessage("§cDas Ändern von Passwörtern ist momentan aus Sicherheitsgründen deaktiviert. " + //Changing of passwords is currently disabled due to
                     "Bitte kontaktiere den Support, falls du deines vergessen haben solltest.");  //security concerns. Contact support if you forgot yours.
             return true;
@@ -89,14 +100,36 @@ public class CommandWebsite extends MTCPlayerOnlyCommandExecutor {
         return true;
     }
 
-    private void setAccountStatus(Player plr, String newStateDescription, boolean enabled) {
+    private boolean hasConfirmedActivate(Player plr, String[] args) {
+        if (args.length < 2 || !args[1].equalsIgnoreCase(playerCodes.get(plr.getUniqueId()))) {
+            String confirmationCode = StringHelper.alphanumericString(8, false).toUpperCase();
+            confirmationCode = String.format("%s-%s", confirmationCode.substring(0, 4), confirmationCode.substring(5, 9));
+            playerCodes.put(plr.getUniqueId(), confirmationCode);
+
+            plr.spigot().sendMessage(new ComponentBuilder("Achtung, du bist gerade in Begriff, deinen " +
+                    "Homepage-Account zu aktivieren!").color(ChatColor.RED).bold(true).create()); //You're about to activate your website account!
+            plr.spigot().sendMessage(new ComponentBuilder("Solltest du nicht gerade einen Account auf " +
+                    "unserer ").color(ChatColor.YELLOW).bold(true) //If you didn't create one at our site,
+                    .append("Webseite (https://minotopia.me/)").underlined(true)
+                    .event(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://www.minotopia.me/")) //vv you're
+                    .append(" registriert haben, bist du möglicherweise Opfer eines Betrugs!").underlined(false).create());
+            plr.spigot().sendMessage(new ComponentBuilder("Ansonsten klicke hier").color(ChatColor.GOLD).underlined(true)
+                    .event(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/hp aktivieren " + confirmationCode))
+                    .append(" oder tippe ").underlined(false).color(ChatColor.YELLOW)
+                    .append("/hp aktivieren " + confirmationCode).color(ChatColor.GOLD).create());
+            return false;
+        }
+        return true;
+    }
+
+    private void setAccountStatus(Player plr, String newStateDescription, boolean enabled, String[] args) {
         int rowsAffected = module.getPlugin().getSql().safelyExecuteUpdate("UPDATE " + WebsiteModule.WEBSITE_USER_TABLE_NAME +
                 " SET status=? WHERE name=?", enabled ? 1 : 0, plr.getName()); //TODO: UUIDs
 
         if (rowsAffected == 1) {
             plr.sendMessage("§aDein Homepageaccount wurde " + newStateDescription); //Your account has been activated, you can log in now!
         } else if (rowsAffected > 1) {
-            plr.sendMessage("§cFür deinen Namen gibt es mehrere Accounts! Kontaktiere bitte den Support, danke!"); //Multipel accounts for your name, contact support.
+            plr.sendMessage("§cFür deinen Namen gibt es mehrere Accounts! Kontaktiere bitte den Support, danke!"); //Multiple accounts for your name, contact support.
         } else { //0 or less
             plr.sendMessage("§cDu hast noch keinen Account registriert! Bitte fülle dieses Formular aus: http://www.minotopia.me/?p=2"); //You don't have any account, please fill this to create one
         }
