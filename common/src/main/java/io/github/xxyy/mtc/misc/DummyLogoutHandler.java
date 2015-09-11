02b26ed4de9d0530e72f441049a3d7a5000a976f
Janmm14 <computerjanimaus@yahoo.de>
2015-09-11 19:58:14 +0200

    Remove implNote javadoc and add @author class javadoc

--- common/src/main/java/io/github/xxyy/mtc/misc/DummyLogoutHandler.java
+++ common/src/main/java/io/github/xxyy/mtc/misc/DummyLogoutHandler.java
@@ -7,26 +7,19 @@ import java.util.UUID;
 
 /**
  * Dummy logout handler used when AntiLogout is disabled.
+ * @author Janmm14
  */
 public class DummyLogoutHandler implements AntiLogoutHandler {
-    /**
-     * @implNote returns always false
-     */
+
     @Override
     public boolean isFighting(UUID uuid) {
         return false;
     }
 
-    /**
-     * @implNote does nothing
-     */
     @Override
     public void setFighting(Player damaged, Player damager, Calendar cal) {
     }
 
-    /**
-     * @implNote does nothing
-     */
     @Override
     public void clearFighters() {
     }
