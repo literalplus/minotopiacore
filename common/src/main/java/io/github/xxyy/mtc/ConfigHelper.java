2bc3883a0c078f10855834a19cb2b4ef4da48a43
xxyy <devnull@nowak-at.net>
2015-12-26 22:10:23 +0100

    Clean up unused values from ConfigHelper

--- common/src/main/java/io/github/xxyy/mtc/ConfigHelper.java
+++ common/src/main/java/io/github/xxyy/mtc/ConfigHelper.java
@@ -9,7 +9,6 @@ package io.github.xxyy.mtc;
 
 import io.github.xxyy.common.util.ChatHelper;
 import io.github.xxyy.mtc.chat.cmdspy.CommandSpyFilters;
-import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
@@ -23,18 +22,13 @@ import java.util.List;
 
 
 public class ConfigHelper {
-    private static short adDetectionLevel;
-    private static boolean adDetectionReplacePointLikeChars;
-    private static int SpeedOnJoinPotency;
     private static String tabListAllowedColors;
-    private static boolean cmdSpyEnabled;
     private static boolean prohibitCmdsInBoats;
     private static boolean enableTablist;
     private static boolean fullLogEnabled;
     private static boolean fullTagEnabled;
     private static boolean mainLoggerEnabled;
     private static List<String> fullTagAllowedPlayers;
-    private static boolean enableMsgOnFirstJoin;
     private static boolean removeUnknownFulls;
     private static ItemStack itemOnJoin;
     private static boolean enableItemOnJoin;
@@ -51,22 +45,15 @@ public class ConfigHelper {
     private static boolean scBReverseSql;
     private static ArrayList<PotionEffect> snowballEffects;
     private static boolean magicSnowballEnabled;
-    private static boolean invseeEnabled;
     private static boolean userStatisticsEnabled;
     private static long snowballTimeoutTicks;
-    private static boolean peaceEnabled;
     private static boolean worldSpecificChat;
     private static boolean antiLogoutEnabled;
     private static int secsInFight;
 	private static int hologramTimeout;
     private static List<String> fightAllowedCmds;
     private static List<String> vehicleAllowedCmds;
-    private static boolean enableBungeeAPI;
     private static List<Integer> brewStackCheckpoints;
-
-    public static short getAdDetectionLevel() {
-        return ConfigHelper.adDetectionLevel;
-    }
 
     public static List<Integer> getAnvilAllowedItems() {
         return ConfigHelper.anvilAllowedItems;
@@ -133,10 +120,6 @@ public class ConfigHelper {
         return ConfigHelper.snowballTimeoutTicks;
     }
 
-    public static int getSpeedOnJoinPotency() {
-        return ConfigHelper.SpeedOnJoinPotency;
-    }
-
     public static String getTabListAllowedColors() {
         return ConfigHelper.tabListAllowedColors;
     }
@@ -160,35 +143,15 @@ public class ConfigHelper {
         ConfigHelper.initClassProperties(cfg);
     }
 
-    public static boolean isAdDetectionReplacePointLikeChars() {
-        return ConfigHelper.adDetectionReplacePointLikeChars;
-    }
-
-    /**
-     * @return the antiLogoutEnabled
-     */
     public static boolean isAntiLogoutEnabled() {
         return ConfigHelper.antiLogoutEnabled;
-    }
-
-    public static boolean isChatUseClan() {
-        return ConfigHelper.chatUseClan;
     }
 
     public static boolean isClanEnabled() {
         return ConfigHelper.clanEnabled;
     }
-
-    public static boolean isCmdSpyEnabled() {
-        return ConfigHelper.cmdSpyEnabled;
-    }
-
     public static boolean isEnableItemOnJoin() {
         return ConfigHelper.enableItemOnJoin;
-    }
-
-    public static boolean isEnableMsgOnFirstJoin() {
-        return ConfigHelper.enableMsgOnFirstJoin;
     }
 
     public static boolean isEnableScB() {
@@ -207,10 +170,6 @@ public class ConfigHelper {
         return ConfigHelper.fullTagEnabled;
     }
 
-    public static boolean isInvseeEnabled() {
-        return ConfigHelper.invseeEnabled;
-    }
-
     public static boolean isMagicSnowballEnabled() {
         return ConfigHelper.magicSnowballEnabled;
     }
@@ -219,16 +178,8 @@ public class ConfigHelper {
         return ConfigHelper.mainLoggerEnabled;
     }
 
-    public static boolean isPeaceEnabled() {
-        return ConfigHelper.peaceEnabled;
-    }
-
     public static boolean isProhibitCmdsInBoats() {
         return ConfigHelper.prohibitCmdsInBoats;
-    }
-
-    public static boolean isRemoveUnknownFulls() {
-        return ConfigHelper.removeUnknownFulls;
     }
 
     public static boolean isScBDisplayPlayerCount() {
@@ -249,10 +200,6 @@ public class ConfigHelper {
 
     public static boolean isWorldSpecificChat() {
         return ConfigHelper.worldSpecificChat;
-    }
-
-    public static boolean isEnableBungeeAPI() {
-        return enableBungeeAPI;
     }
 
     public static List<Integer> getBrewStackCheckpoints() {
@@ -279,16 +226,13 @@ public class ConfigHelper {
     }
 
     private static void addClanDefaults(FileConfiguration cfg) {
-//        cfg.addDefault("", "");
         cfg.addDefault("clan.maxusers", 15);
         cfg.addDefault("clan.maxusersextended", 25);
     }
 
     private static void addEnableDefaults(FileConfiguration cfg) {
         cfg.addDefault("enable.mtc", true);
-        cfg.addDefault("enable.falldown", true);
         cfg.addDefault("enable.chat", true);
-        cfg.addDefault("enable.teambattle", true);
         cfg.addDefault("enable.dmgevent", true);
         cfg.addDefault("enable.command.lore", true);
         cfg.addDefault("enable.command.gtime", true);
@@ -296,11 +240,9 @@ public class ConfigHelper {
         cfg.addDefault("enable.command.playerhead.getall-op-notify", true);
         cfg.addDefault("enable.command.breload", true);
         cfg.addDefault("enable.command.mtc", true);
-        cfg.addDefault("enable.command.war", true);
         cfg.addDefault("enable.command.invsee", true);
         cfg.addDefault("enable.command.team", true);
         cfg.addDefault("enable.command.rename", true);
-        cfg.addDefault("enable.bungeeapi", true);
         cfg.addDefault("enable.misc.lighting.cow", true);
         cfg.addDefault("enable.help", true);
         cfg.addDefault("enable.msg.enablePlug", true);
@@ -316,23 +258,18 @@ public class ConfigHelper {
         cfg.addDefault("enable.betterdmgpotions", true);
         cfg.addDefault("enable.speedonjoin", false);
         cfg.addDefault("enable.cmdspy", true);
-        cfg.addDefault("enable.noobprotection", true);
         cfg.addDefault("enable.fulltag", true);
         cfg.addDefault("enable.log.main", true);
         cfg.addDefault("enable.log.cmds", true);
         cfg.addDefault("enable.log.chat", true);
-        cfg.addDefault("enable.log.warnban", true);
-        cfg.addDefault("enable.log.fulls", true);
         cfg.addDefault("enable.fixes.minecartPortal", true);
         cfg.addDefault("enable.enderpearlListener", true);
         cfg.addDefault("enable.fixes.freecam", true);
         cfg.addDefault("enable.fixes.minecartPortal", true);
         cfg.addDefault("enable.fixes.boatCmds", true);
-        cfg.addDefault("enable.msgOnFirstJoin", true);
-        cfg.addDefault("enable.itemonjoin", false);
         cfg.addDefault("enable.clan", true);
         cfg.addDefault("enable.stats", true);
-        cfg.addDefault("enable.scoreboard", true);
+        cfg.addDefault("enable.scoreboard", false);
         cfg.addDefault("enable.snowball", true);
         cfg.addDefault("enable.userstatistics", true);
         cfg.addDefault("enable.signcolor", true);
@@ -344,12 +281,6 @@ public class ConfigHelper {
     }
 
     private static void addFixDefaults(FileConfiguration cfg) {
-        cfg.addDefault("fixes.netherroof.spawn.worldName", "world");
-        cfg.addDefault("fixes.netherroof.spawn.x", 0);
-        cfg.addDefault("fixes.netherroof.spawn.y", 70);
-        cfg.addDefault("fixes.netherroof.spawn.z", 0);
-        cfg.addDefault("fixes.netherroof.spawn.pitch", 0);
-        cfg.addDefault("fixes.netherroof.spawn.yaw", 0);
         cfg.addDefault("fixes.anvil-allowed-item-ids", Arrays.asList(0, 256, 257, 261, 267, 264,
                 265, 266, 268, 269, 270, 272, 273, 274, 275, 276, 277, 278, 283, 285, 284, 298, 299, 300, 301,
                 302, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, 315, 316, 317));
@@ -359,15 +290,12 @@ public class ConfigHelper {
     private static void addMiscDefaults(FileConfiguration cfg) {
         cfg.addDefault("badCmds", Arrays.asList("op", "gamemode", "full", "banip"));
         cfg.addDefault("tablist.allowedColors", "0123456789abcdef");
-        cfg.addDefault("warnban.serversuffix", "§7§o[UnknownServer]");
         cfg.addDefault("motd", "&6&lMinoTopia.me");
         cfg.addDefault("speedonjoin.potency", 5);
         cfg.addDefault("servername", "Unknown-" + (new File("").getAbsolutePath()));
-        cfg.addDefault("noobprotection.durationInMinutes", 20);
         cfg.addDefault("fulltag.allowedPlayers", Arrays.asList("xxyy98", "chris301234"));
         cfg.addDefault("fulltag.removeUnkownFulls", false);
         cfg.addDefault("fulltag.checkEveryInMinutes", 20);
-        cfg.addDefault("itemonjoin", new ItemStack(Material.WATCH));
         cfg.addDefault("scoreboard.updateIntervalTicks", 100);
         cfg.addDefault("scoreboard.mode", "ALL");
         cfg.addDefault("scoreboard.modeExplanation", "ALL=cycle through; PVP=only PvP; TM=only TM");
@@ -394,19 +322,14 @@ public class ConfigHelper {
     }
 
     private static void initClassProperties(FileConfiguration cfg) {
-        ConfigHelper.adDetectionLevel = (short) cfg.getInt("chat.adDetectionLevel");
-        ConfigHelper.adDetectionReplacePointLikeChars = cfg.getBoolean("chat.adDetectionReplacepointLikeChars");
         ChatHelper.percentForCaps = cfg.getInt("chat.maxCapsInPercent", 50);
-        ConfigHelper.SpeedOnJoinPotency = (cfg.getBoolean("enable.speedonjoin", false) ? (cfg.getInt("speedonjoin.potency", 5) - 1) : -1);
         ConfigHelper.tabListAllowedColors = cfg.getString("tablist.allowedColors", "7012345689abcdef");
-        ConfigHelper.cmdSpyEnabled = cfg.getBoolean("enable.cmdspy", true);
         ConfigHelper.prohibitCmdsInBoats = cfg.getBoolean("enable.fixes.boatCmds", true);
         ConfigHelper.enableTablist = cfg.getBoolean("enable.tablist", true);
         ConfigHelper.fullLogEnabled = cfg.getBoolean("enable.log.fulls", true);
         ConfigHelper.fullTagEnabled = cfg.getBoolean("enable.fulltag", true);
         ConfigHelper.mainLoggerEnabled = cfg.getBoolean("enable.log.main", true);
         ConfigHelper.fullTagAllowedPlayers = cfg.getStringList("fulltag.allowedPlayers");
-        ConfigHelper.enableMsgOnFirstJoin = cfg.getBoolean("enable.msgOnFirstJoin", true);
         ConfigHelper.removeUnknownFulls = cfg.getBoolean("fulltag.removeUnkownFulls", false);
         ConfigHelper.itemOnJoin = cfg.getItemStack("itemonjoin");
         ConfigHelper.enableItemOnJoin = cfg.getBoolean("enable.itemonjoin");
@@ -422,10 +345,8 @@ public class ConfigHelper {
         ConfigHelper.scBDisplayPlayerCount = cfg.getBoolean("scoreboard.showPlayerCount", false);
         ConfigHelper.scBReverseSql = cfg.getBoolean("scoreboard.reversesql", false);
         ConfigHelper.initPotionProps(cfg);
-        ConfigHelper.invseeEnabled = cfg.getBoolean("enable.command.invsee", true);
         ConfigHelper.userStatisticsEnabled = cfg.getBoolean("enable.userstatistics", true);
         ConfigHelper.snowballTimeoutTicks = cfg.getLong("snowball.timeoutTicks", 12000L);
-        ConfigHelper.peaceEnabled = cfg.getBoolean("enable.peace", false);
         ConfigHelper.worldSpecificChat = cfg.getBoolean("enable.worldspecificchat", false);
         ConfigHelper.antiLogoutEnabled = cfg.getBoolean("enable.antilogout", false);
         ConfigHelper.secsInFight = cfg.getInt("antilogout.secsInFight", 20);
@@ -433,7 +354,6 @@ public class ConfigHelper {
         ConfigHelper.fightAllowedCmds = cfg.getStringList("antilogout.allowedCmds");
         ChatHelper.allowedChatColors = cfg.getString("chat.farbe.allowed", "012356789AaBbDdEeFfRr");
         ConfigHelper.vehicleAllowedCmds = cfg.getStringList("vehicles.allowedCmds");
-        ConfigHelper.enableBungeeAPI = cfg.getBoolean("enable.bungeeapi");
         setBrewStackCheckpoints(cfg.getIntegerList("fixes.brewstack.checkpoints"));
         cfg.getStringList("badCmds").stream().forEach(CommandSpyFilters::addBadCommand);
     }
@@ -454,7 +374,6 @@ public class ConfigHelper {
                     ConfigHelper.snowballEffects.add(new PotionEffect(PotionEffectType.ABSORPTION, 40, 2));//2s
                     break;
                 default:
-//                PotionEffectType type = PotionEffectType.getById(i);
                     PotionEffectType type = PotionEffectType.getByName(typeName);
                     if (type == null) {
                         continue;
