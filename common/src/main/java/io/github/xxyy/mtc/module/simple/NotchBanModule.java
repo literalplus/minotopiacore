2710e26a59a99414cec0812bad03dcd740534af1
xxyy <devnull@nowak-at.net>
2016-01-10 01:50:29 +0100

    Add NotchBanModule, which blocks golden apples from being crafted

---
+++ common/src/main/java/io/github/xxyy/mtc/module/simple/NotchBanModule.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2013-2016.
+ * This work is protected by international copyright laws and licensed
+ * under the license terms which can be found at src/main/resources/LICENSE.txt
+ * or alternatively obtained by sending an email to xxyy98+mtclicense@gmail.com.
+ */
+
+package io.github.xxyy.mtc.module.simple;
+
+import io.github.xxyy.mtc.api.MTCPlugin;
+import io.github.xxyy.mtc.module.MTCModuleAdapter;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+
+/**
+ * Disallows the crafting of any kind of golden apple.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 2016-01-10
+ */
+public class NotchBanModule extends MTCModuleAdapter {
+    private NotchListener listener;
+
+    protected NotchBanModule() {
+        super("NotchBan", false);
+    }
+
+    @Override
+    public void enable(MTCPlugin plugin) throws Exception {
+        super.enable(plugin);
+        listener = new NotchListener();
+        plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+    }
+
+    @Override
+    public void disable(MTCPlugin plugin) {
+        super.disable(plugin);
+        HandlerList.unregisterAll(listener);
+    }
+
+    private class NotchListener implements Listener {
+        @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+        public void onPrepareItemCraft(CraftItemEvent evt) {
+            if (evt.getRecipe().getResult().getType().equals(Material.GOLDEN_APPLE)) {
+                evt.getViewers().forEach(humanEntity ->
+                        humanEntity.sendMessage("§cAuf MinoTopia kannst du keine goldenen Äpfel craften! " +
+                                "Konsultiere die Händler des Schreckens am Spawn."));
+                evt.setCancelled(true);
+            }
+        }
+    }
+}
---
+++ common/src/main/java/io/github/xxyy/mtc/module/simple/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * A package for simple, one-class MTC modules.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 2016-01-10
+ */
+package io.github.xxyy.mtc.module.simple;
