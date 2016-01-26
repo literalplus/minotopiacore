df7872e4a5983687a71fa6b042f3a38d2b082d64
Janmm14 <computerjanimaus@yahoo.de>
2016-01-26 14:34:10 +0100

    Tidy up JavaUtils class.

--- common/src/main/java/io/github/xxyy/mtc/misc/JavaUtils.java
+++ common/src/main/java/io/github/xxyy/mtc/misc/JavaUtils.java
@@ -1,29 +1,32 @@
 package io.github.xxyy.mtc.misc;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.Predicate;
 
 /**
- * Provides utilities for the
+ * Provides utilities for basic java classes
  */
-public class JavaUtils {
+public final class JavaUtils {
     private JavaUtils() {
         throw new UnsupportedOperationException("Instantiation of utility class");
     }
 
     /**
-     * Negates a predicate.<br>
-     * <br>
+     * Negates a predicate.
+     * <p>
      * You simply can't just do things like this easily:
-     * {@code (StringUtils::isNumeric).negate()}<br>
-     * <br>
-     * For the sane of the code, this function is available and<br>
+     * {@code (StringUtils::isNumeric).negate()}
+     * <p>
+     * For the sane of the code, this function is available and
      * you can import this functon static for even nicer code.
      *
      * @param predicate predicate to negate
      * @param <T>       {@link Predicate} (the type of the input to the predicate)
      * @return the negated predicate
      */
-    public static <T> Predicate<T> not(Predicate<T> predicate) {
+    @NotNull
+    public static <T> Predicate<T> not(@NotNull Predicate<T> predicate) {
         return predicate.negate();
     }
 }
