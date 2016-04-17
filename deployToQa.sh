6ce678f462a2a40a64a9221280613987bd32b96b
xxyy <devnull@nowak-at.net>
2016-04-18 00:11:08 +0200

    Add bash script for quick QA deployment

---
+++ deployToQa.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Temporarily deploys this workspace's MTC version to mtc-qa
+# Re-run Jenkins build to reset environment
+# For this to work, you need to have mt-mikey defined in your /etc/hosts
+
+echo "Building..."
+mvn clean install
+echo "Built..."
+
+echo "Deploying..."
+cd bootstrap/target
+scp minotopaicore-*.jar mt-mikey:/home/minecraft/plugins/qa/mtc.jar
+scp minotopaicore-*.jar mt-mikey:/home/minecraft/mtc-qa/plugins/mtc.jar
+
+echo "Deployed..."
+echo "Done!"
