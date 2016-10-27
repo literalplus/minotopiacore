3e2bbaa4aaa2d8cf95b66df3bd668764cdf2aa6c
xxyy <devnull@nowak-at.net>
2016-10-27 23:13:07 +0200

    Change /lagive to use CreditMelonsBuilder API

--- common/src/main/java/li/l1t/mtc/module/lanatus/base/LanatusGiveCommand.java
+++ common/src/main/java/li/l1t/mtc/module/lanatus/base/LanatusGiveCommand.java
@@ -11,8 +11,7 @@ import li.l1t.common.exception.InternalException;
 import li.l1t.common.exception.UserException;
 import li.l1t.lanatus.api.LanatusClient;
 import li.l1t.lanatus.api.account.AccountSnapshot;
-import li.l1t.lanatus.api.account.MutableAccount;
-import li.l1t.lanatus.api.exception.AccountConflictException;
+import li.l1t.lanatus.api.account.LanatusAccount;
 import li.l1t.mtc.api.chat.MessageType;
 import li.l1t.mtc.api.command.CommandExecution;
 import li.l1t.mtc.command.BukkitExecutionExecutor;
@@ -55,9 +54,9 @@ public class LanatusGiveCommand extends BukkitExecutionExecutor {
 
     private void handleGiveMelons(CommandExecution exec, XLoginHook.Profile profile, int amount) {
         respondOperationStart(exec, profile, amount);
-        MutableAccount account = client.accounts().findMutable(profile.getUniqueId());
+        LanatusAccount account = client.accounts().find(profile.getUniqueId());
         checkTransactionPossible(amount, account);
-        tryModifyMelonsCount(amount, account);
+        tryModifyMelonsCount(exec, account, amount);
         respondSuccess(exec, profile, account);
     }
 
@@ -66,33 +65,31 @@ public class LanatusGiveCommand extends BukkitExecutionExecutor {
                 profile.getName(), amount);
     }
 
-    private void checkTransactionPossible(int amount, MutableAccount account) {
+    private void checkTransactionPossible(int amount, LanatusAccount account) {
         int expectedFinalCount = account.getMelonsCount() + amount;
         if (expectedFinalCount < 0) {
             throw new UserException(
-                    "Nach dieser Aktion hätte %s negative Melonen! (%d - %d = %d)",
+                    "Nach dieser Aktion hätte diese Person negative Melonen! (%d - %d = %d)",
                     account.getMelonsCount(), amount * -1, expectedFinalCount
             );
         }
     }
 
-    private void tryModifyMelonsCount(int amount, MutableAccount account) {
-        account.modifyMelonsCount(amount);
-        try {
-            client.accounts().save(account);
-        } catch (AccountConflictException e) {
-            throw new InternalException("Der Account wurde anderswo geändert. Bitte versuche es erneut.", e);
-        }
+    private void tryModifyMelonsCount(CommandExecution exec, LanatusAccount account, int amount) {
+        client.creditMelons(account.getPlayerId())
+                .withComment(String.format("/lagive von %s (%s)", exec.senderName(), exec.senderId()))
+                .withMelonsCount(amount)
+                .build();
     }
 
-    private void respondSuccess(CommandExecution exec, XLoginHook.Profile profile, MutableAccount account) {
+    private void respondSuccess(CommandExecution exec, XLoginHook.Profile profile, LanatusAccount account) {
         AccountSnapshot remoteAccount = client.accounts().find(profile.getUniqueId());
         exec.respond(MessageType.RESULT_LINE_SUCCESS, "Aktion erfolgreich! %s hat jetzt %d Melonen.",
                 profile.getName(), remoteAccount.getMelonsCount());
         checkChangedConcurrently(exec, account, remoteAccount);
     }
 
-    private void checkChangedConcurrently(CommandExecution exec, MutableAccount account, AccountSnapshot remoteAccount) {
+    private void checkChangedConcurrently(CommandExecution exec, LanatusAccount account, AccountSnapshot remoteAccount) {
         if (account.getMelonsCount() != remoteAccount.getMelonsCount()) {
             exec.respond(MessageType.WARNING, "Der Account wurde währenddessen woanders geändert.");
         }
