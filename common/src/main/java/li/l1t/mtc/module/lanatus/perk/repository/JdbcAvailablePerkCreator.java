1f0e03690b752c673f868bd92fb521c3b6e4cc03
Literallie <devnull@nowak-at.net>
2016-12-07 16:11:28 +0100

    Fix NPE on /perks; #552

--- common/src/main/java/li/l1t/mtc/module/lanatus/perk/repository/JdbcAvailablePerkCreator.java
+++ common/src/main/java/li/l1t/mtc/module/lanatus/perk/repository/JdbcAvailablePerkCreator.java
@@ -11,6 +11,8 @@ import li.l1t.mtc.module.lanatus.base.product.JdbcProductMetadataCreator;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.Instant;
 
 /**
  * Creates available perk metadata from JDBC result sets.
@@ -22,8 +24,13 @@ class JdbcAvailablePerkCreator extends JdbcProductMetadataCreator<AvailablePerk>
     @Override
     public AvailablePerk createFromCurrentRow(ResultSet rs) throws SQLException {
         return new AvailablePerk(
-                productId(rs), rs.getTimestamp("validuntil").toInstant(),
+                productId(rs), nullableTimestamp(rs, "validuntil"),
                 uuid(rs, "player_id")
         );
+    }
+
+    private Instant nullableTimestamp(ResultSet rs, String column) throws SQLException {
+        Timestamp value = rs.getTimestamp(column);
+        return value == null ? null : value.toInstant();
     }
 }
