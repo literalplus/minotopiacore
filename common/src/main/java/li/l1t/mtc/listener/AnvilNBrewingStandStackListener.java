035b90e1ef570af3acb35f8a213df34c96c0780a
Literallie <devnull@nowak-at.net>
2016-12-12 16:03:08 +0100

    Add experimental fix for stacked brewing in 1.9+; Fixes #660

--- common/src/main/java/li/l1t/mtc/listener/AnvilNBrewingStandStackListener.java
+++ common/src/main/java/li/l1t/mtc/listener/AnvilNBrewingStandStackListener.java
@@ -7,6 +7,7 @@
 
 package li.l1t.mtc.listener;
 
+import com.google.common.base.Preconditions;
 import li.l1t.mtc.ConfigHelper;
 import li.l1t.mtc.MTC;
 import org.bukkit.Material;
@@ -14,10 +15,14 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.BrewEvent;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.BrewerInventory;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.PotionMeta;
 
 public class AnvilNBrewingStandStackListener implements Listener {
 
@@ -28,7 +33,7 @@ public class AnvilNBrewingStandStackListener implements Listener {
         }
         HumanEntity he = e.getWhoClicked();
         if (!(he instanceof Player)) {
-            return; //lol
+            return; //casually implying this will ever happen
         }
         Player plr = (Player) he;
         Inventory inv = e.getInventory();
@@ -61,26 +66,83 @@ public class AnvilNBrewingStandStackListener implements Listener {
 
     }
 
+    @EventHandler
+    public void onBrew(BrewEvent event) {
+        BrewerInventory inventory = event.getContents();
+        int[] slotAmounts = new int[3];
+        for (int i = 0; i < 3; i++) {
+            ItemStack item = inventory.getItem(i);
+            if (item != null) {
+                slotAmounts[i] = item.getAmount();
+            }
+        }
+        MTC plugin = MTC.instance();
+        plugin.getServer().getScheduler().runTask(plugin, () -> {
+            for (int i = 0; i < 3; i++) {
+                if (slotAmounts[i] > 1) {
+                    ItemStack result = inventory.getItem(i);
+                    result.setAmount(slotAmounts[i]);
+                    inventory.setItem(i, result);
+                }
+            }
+        });
+    }
+
     @EventHandler(ignoreCancelled = true)
-    public void onInvClickBrewingStand(InventoryClickEvent e) {
-        HumanEntity he = e.getWhoClicked();
-        if (!(he instanceof Player)) {
+    public void onInvClickBrewingStand(InventoryClickEvent event) {
+        HumanEntity humanEntity = event.getWhoClicked();
+        if (!(humanEntity instanceof Player)) {
             return; //lol
         }
-        Player plr = (Player) he;
-        Inventory inv = e.getInventory();
-        if (!inv.getType().equals(InventoryType.BREWING)) {
-            return;
+        Player player = (Player) humanEntity;
+        System.out.println(event.getAction() + " with " + event.getCursor() + " slot=" + event.getRawSlot() + " previtem=" + event.getCurrentItem());
+        if (isClickInPotionSlotOfBrewingStandWithItemOnCursor(event)) {
+            handlePotionSlotClick(event, player);
         }
+    }
 
-        int maxAmount = getPermittedStackAmount(plr);
+    private boolean isClickInPotionSlotOfBrewingStandWithItemOnCursor(InventoryClickEvent e) {
+        return e.getClickedInventory() != null && e.getClickedInventory().getType().equals(InventoryType.BREWING) &&
+                e.getCursor() != null && e.getRawSlot() <= 3;
+    }
 
-        if ((e.getCursor() != null && e.getCursor().getAmount() > maxAmount) || (e.getCurrentItem() != null && e.getCurrentItem().getAmount() > maxAmount)
-                || e.getAction().equals(InventoryAction.HOTBAR_SWAP) || e.getAction().equals(InventoryAction.HOTBAR_MOVE_AND_READD)) {
-            plr.sendMessage(MTC.chatPrefix + "Du darfst im Braustand keine gestackten Items verwenden.");
-            plr.closeInventory();
-            e.setCancelled(true);
+    private void handlePotionSlotClick(InventoryClickEvent event, Player player) {
+        if (event.getAction() == InventoryAction.PLACE_ALL && areCursorAndSlotItemMergeable(event)) {
+            mergeCursorWithSlotItem(event, getPermittedStackAmount(player));
         }
+    }
+
+    private boolean areCursorAndSlotItemMergeable(InventoryClickEvent event) {
+        ItemStack currentItem = event.getCurrentItem();
+        ItemStack cursor = event.getCursor();
+        Preconditions.checkNotNull(cursor, "cursor");
+        if (currentItem == null || currentItem.getType().equals(Material.AIR)) {
+            return true;
+        } else if (currentItem.getType().equals(cursor.getType())) {
+            if (currentItem.getItemMeta() instanceof PotionMeta && cursor.getItemMeta() instanceof PotionMeta) {
+                return currentItem.getItemMeta().equals(cursor.getItemMeta());
+            }
+        }
+        return false;
+    }
+
+    private void mergeCursorWithSlotItem(InventoryClickEvent event, int maxAllowedStackSize) {
+        ItemStack cursor = event.getCursor();
+        ItemStack currentItem = event.getCurrentItem();
+        int totalAmountOfItems = cursor.getAmount() + currentItem.getAmount();
+        int effectiveStackSize = Math.min(totalAmountOfItems, maxAllowedStackSize);
+        event.setCurrentItem(setStackAmount(cursor, effectiveStackSize));
+        event.setCursor(setStackAmount(cursor, totalAmountOfItems - effectiveStackSize));
+        event.setCancelled(true);
+    }
+
+    private ItemStack setStackAmount(ItemStack baseStack, int amount) {
+        if(amount <= 0) {
+            return new ItemStack(Material.AIR, 0);
+        }
+        ItemStack cloned = baseStack.clone();
+        cloned.setAmount(amount);
+        return cloned;
     }
 
     private int getPermittedStackAmount(Player plr) {
