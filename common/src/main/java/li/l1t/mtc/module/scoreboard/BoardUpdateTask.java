a5f833126f914bd67fc8b27a3a272555d93dd635
Literallie <devnull@nowak-at.net>
2016-12-15 08:21:12 +0100

    Also update scoreboard immediately after reload/restart

--- common/src/main/java/li/l1t/mtc/module/scoreboard/BoardUpdateTask.java
+++ common/src/main/java/li/l1t/mtc/module/scoreboard/BoardUpdateTask.java
@@ -29,7 +29,7 @@ public class BoardUpdateTask extends ImprovedBukkitRunnable {
     }
 
     public void start(long period) {
-        runTaskTimerAsynchronously(plugin, period);
+        runTaskTimerAsynchronously(plugin, 0L, period);
     }
 
     public void restart(long newPeriod) {
