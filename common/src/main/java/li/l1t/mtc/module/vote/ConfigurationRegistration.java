08841fdbc4dfd0936b8ea7a813cc75441ad7a09c
Literallie <devnull@nowak-at.net>
2017-01-17 14:38:45 +0100

    Actually register MessageReward

--- common/src/main/java/li/l1t/mtc/module/vote/ConfigurationRegistration.java
+++ common/src/main/java/li/l1t/mtc/module/vote/ConfigurationRegistration.java
@@ -10,6 +10,7 @@ package li.l1t.mtc.module.vote;
 import com.google.common.collect.ImmutableList;
 import li.l1t.mtc.module.vote.reward.CommandReward;
 import li.l1t.mtc.module.vote.reward.ItemReward;
+import li.l1t.mtc.module.vote.reward.MessageReward;
 import li.l1t.mtc.module.vote.reward.compound.*;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.inventory.ItemStack;
@@ -24,7 +25,8 @@ class ConfigurationRegistration {
     static void registerAll() {
         ImmutableList.of(
                 CommandReward.class, ItemReward.class, StreakCompoundReward.class, WeekdayCompoundReward.class,
-                RandomCompoundReward.class, AllOfCompoundReward.class, WeightedRewardWrapper.class
+                RandomCompoundReward.class, AllOfCompoundReward.class, WeightedRewardWrapper.class,
+                MessageReward.class
         ).forEach(ConfigurationSerialization::registerClass);
         ConfigurationSerialization.registerClass(ItemStack.class, "mtc.itemstack");
     }
