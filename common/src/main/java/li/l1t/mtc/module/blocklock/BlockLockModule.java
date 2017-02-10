81d5f40723c583849f4aef1b57f90a6cfa3cf700
Literallie <devnull@nowak-at.net>
2017-02-10 14:50:29 +0100

    Add /bl tool command and related listener

--- common/src/main/java/li/l1t/mtc/module/blocklock/BlockLockModule.java
+++ common/src/main/java/li/l1t/mtc/module/blocklock/BlockLockModule.java
@@ -14,6 +14,8 @@ import li.l1t.mtc.module.ConfigurableMTCModule;
 import li.l1t.mtc.module.blocklock.command.BlockLockCommand;
 import li.l1t.mtc.module.blocklock.listener.BlockLockInteractionListener;
 import li.l1t.mtc.module.blocklock.listener.BlockLockPlaceListener;
+import li.l1t.mtc.module.blocklock.listener.BlockLockToolListener;
+import org.bukkit.Material;
 
 /**
  * A module that adds special protection for selected materials, allowing only the player who placed it to destroy it.
@@ -24,6 +26,8 @@ import li.l1t.mtc.module.blocklock.listener.BlockLockPlaceListener;
 public class BlockLockModule extends ConfigurableMTCModule {
     public static final String INFO_PERMISSION = "mtc.blocklock.info";
     public static final String ADMIN_PERMISSION = "mtc.blocklock.admin";
+    public static final String TOOL_DISPLAY_NAME = "§3§lBlockLock-Infotool";
+    public static final Material TOOL_TYPE = Material.MELON_BLOCK;
     private BlockLockConfig config;
 
     @InjectMe
@@ -38,6 +42,7 @@ public class BlockLockModule extends ConfigurableMTCModule {
         registerCommand(inject(BlockLockCommand.class), "bl");
         registerListener(inject(BlockLockInteractionListener.class));
         registerListener(inject(BlockLockPlaceListener.class));
+        registerListener(inject(BlockLockToolListener.class));
     }
 
     @Override
--- common/src/main/java/li/l1t/mtc/module/blocklock/command/BlockLockCommand.java
+++ common/src/main/java/li/l1t/mtc/module/blocklock/command/BlockLockCommand.java
@@ -12,8 +12,11 @@ import li.l1t.common.exception.InternalException;
 import li.l1t.common.exception.UserException;
 import li.l1t.common.misc.XyLocation;
 import li.l1t.common.util.LocationHelper;
+import li.l1t.common.util.inventory.ItemStackFactory;
+import li.l1t.mtc.api.chat.MessageType;
 import li.l1t.mtc.api.module.inject.InjectMe;
 import li.l1t.mtc.command.MTCExecutionExecutor;
+import li.l1t.mtc.module.blocklock.BlockLockModule;
 import li.l1t.mtc.module.blocklock.service.BlockLockService;
 
 /**
@@ -37,6 +40,16 @@ public class BlockLockCommand extends MTCExecutionExecutor {
                 case "destroy":
                     XyLocation location = LocationHelper.deserialize(exec.arg(1));
                     lockService.destroyLockAndReturn(location.getBlock(), exec.player());
+                    return true;
+                case "tool":
+                    exec.player().getInventory().addItem(
+                            new ItemStackFactory(BlockLockModule.TOOL_TYPE)
+                                    .displayName(BlockLockModule.TOOL_DISPLAY_NAME)
+                                    .produce()
+                    );
+                    exec.respond(MessageType.RESULT_LINE_SUCCESS,
+                            "Mit diesem Block kannst du inspizieren, wo ein geschützter Block war.");
+                    return true;
             }
         }
         return true;
